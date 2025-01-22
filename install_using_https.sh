cae8874f94fb198845f80ab441151fae6d6a997a
Jacob Long <jacoblong853@gmail.com>
2025-01-22 16:35:59 -0500

    [Install] add vaudeo repo

--- install_using_https.sh
+++ install_using_https.sh
@@ -7,5 +7,10 @@ if [ "$CURR_DIR" != "oricomp" ]; then
     cd oricomp || exit
 fi
 
+# general repositories
 git clone https://github.com/oricomp/.github.git || true
 git clone https://github.com/oricomp/docs.git || true
+
+# hardware repositories
+mkdir -p hardware
+git -C ./hardware clone https://github.com/oricomp/vaudeo.git || true
--- install_using_ssh.sh
+++ install_using_ssh.sh
@@ -7,5 +7,10 @@ if [ "$CURR_DIR" != "oricomp" ]; then
     cd oricomp || exit
 fi
 
+# general repositories
 git clone git@github.com:oricomp/.github.git || true
 git clone git@github.com:oricomp/docs.git || true
+
+# hardware repositories
+mkdir -p hardware
+git -C ./hardware clone git@github.com:oricomp/vaudeo.git || true
